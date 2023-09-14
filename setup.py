a76efe74dfe397950b341ad050fa0f91eacd251f
Abiodun Sulaiman <hello@abiodun.dev>
2023-09-15 00:45:19 +0200

    Update license and author details

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='pyaudiotoolbox',
     version='0.1',
+    author='Abiodun Sulaiman',
+    author_email='hello@abiodun.dev',
+    description='Toolbox for basic audio editing and manipulations',
     packages=find_packages(),
     install_requires=[
         'pydub',
